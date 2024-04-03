b5776be7c447fd018c8b6638a643eef2163c3e91
avish <avishusa45@gmail.com>
2024-04-03 00:59:42 -0500

    Database Connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose.connect(process.env.x).then(()=>{
+    console.log("Connected to MongoDB")
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 
 app.listen(8801, ()=>{
     console.log('Server is running on port 8801')
 });
+
+
