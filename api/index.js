145dbd3c65a9ec83f49212264ed07254ccf8a891
avish <avishusa45@gmail.com>
2024-04-03 01:39:27 -0500

    testing api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import router from './routes/user.route.js';
 
 dotenv.config();
 mongoose.connect(process.env.x).then(()=>{
@@ -15,4 +16,9 @@ app.listen(8801, ()=>{
     console.log('Server is running on port 8801')
 });
 
+app.get('/test',(req,res)=>{
 
+    res.send("Avish")
+})
+
+app.use("/api/user",router);
