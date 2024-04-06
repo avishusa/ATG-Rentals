9623e823c0c08903b8eb07e04cfc9af03de01839
avish <avishusa45@gmail.com>
2024-04-06 14:13:00 -0500

    Created middleware and custom error-handlerfunction for error-handling.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import router from './routes/user.route.js';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 
 dotenv.config();
 mongoose.connect(process.env.x).then(()=>{
@@ -21,4 +23,19 @@ app.get('/test',(req,res)=>{
     res.send("Avish")
 })
 
-app.use("/api/user",router);
+app.use(express.json);
+
+app.use("/api/user",userRouter);
+
+app.use("/api/auth",authRouter);
+
+app.use((req,res,err,next)=>{
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Message";
+    return res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message,
+    })
+})
