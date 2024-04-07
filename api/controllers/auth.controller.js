646e2972e5c52057a887e079100520297a24334e
avish <avishusa45@gmail.com>
2024-04-07 14:24:41 -0500

    Sign-in Page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 // import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import {errorHandler} from '../utils/error.js'
 
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
@@ -9,6 +11,22 @@ export const signup = async (req, res,next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully !");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res,next) => {
+  const { username,password } = req.body;
+  // const hashedPassword = bcryptjs.hashSync(password, 10);
+  try {
+    const validUser = await User.findOne({username});
+    if(!validUser) return next(errorHandler(404,'User Not Found !'))
+    const validPass = await User.findOne({password});
+    if(!validPass) return next(errorHandler(401,'Invalid Password !'))
+    const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+    const {password : pass,...rest} = validUser._doc;
+    res.cookie('access_token',token,{httpOnly : true}).status(200).json(rest)
   } catch (error) {
     next(error);
   }
