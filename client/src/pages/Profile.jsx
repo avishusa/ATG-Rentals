003036225095690d64b30e9f59c9378f96d68415
avish <avishusa45@gmail.com>
2024-04-20 15:08:14 -0500

    Delete user functionality is added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,9 @@ import {
   updateUserFailure,
   updateUserSuccess,
   updateUserStart,
+  deleteUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
 } from "../redux/user/userSlice.js";
 
 function Profile() {
@@ -30,6 +33,23 @@ function Profile() {
       handleFileUpload(file);
     }
   }, [file]);
+
+  const handleDeleteUser = async()=>{
+    try {
+      dispatch(deleteUserStart());
+      const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+        method : 'DELETE',
+      })
+      const data = await res.json();
+      if(data.success===false){
+        dispatch(deleteUserFailure(data.message));
+        return
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
 
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
@@ -143,7 +163,7 @@ function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer" onClick={handleDeleteUser}>Delete Account</span>
         <span className="text-red-700 cursor-pointer">Signout</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ''}</p>
