698a2e6d3a66158b1f7d18c4b7a524b48e7aae06
avish <avishusa45@gmail.com>
2024-04-29 21:51:15 -0500

    Update listing functionality added

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -30,7 +30,7 @@ export const deleteListing = async(req,res)=>{
     }
 }
 
-export const updateListing = async()=>{
+export const updateListing = async(req,res,next)=>{
 
     const listing = await Listing.findById(req.params.id);
     if(!listing){
@@ -46,6 +46,18 @@ export const updateListing = async()=>{
             {new:true}
         );
         res.status(200).json(updateListing)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getListing = async (req,res,next)=>{
+    try {
+        const listing = await Listing.findById((req.params.id));
+        if(!listing){
+            return next(errorHandler(404,'Listing not found'));
+        }
+        res.status(200).json(listing);
     } catch (error) {
         next(error)
     }
