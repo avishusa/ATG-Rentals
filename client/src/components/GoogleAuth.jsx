8e1d87a5818167463313099c9eee64c6115841ab
avish <avishusa45@gmail.com>
2024-05-03 20:57:09 -0500

    Formatting the documetns

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -1,39 +1,46 @@
-import React from 'react'
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
-import { app } from '../firebase'
-import {useDispatch} from 'react-redux';
-import { signInSuccess } from '../redux/user/userSlice';
-import { useNavigate} from 'react-router-dom';
+import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 function GoogleAuth() {
   const dispatch = useDispatch();
-  const navigate =useNavigate();
-  const handleClick = async ()=>{
-
+  const navigate = useNavigate();
+  const handleClick = async () => {
     try {
-      const provider = new GoogleAuthProvider()
-      const auth = getAuth(app)
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
 
-      const result = await signInWithPopup(auth,provider)
-      const res = await fetch('/api/auth/google-signup',{
-        method : 'POST',
-        headers : {
-          'Content-Type':'application/json'
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch("/api/auth/google-signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({name:result.user.displayName,email:result.user.email,photo:result.user.photoURL})
-      })
-      const data=await res.json()
-      dispatch(signInSuccess(data))
-      navigate('/')
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      console.log('Could not sign in with google',error)
+      console.log("Could not sign in with google", error);
     }
-  }
+  };
   return (
-    <button onClick={handleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'> 
+    <button
+      onClick={handleClick}
+      type="button"
+      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+    >
       Login with Google
     </button>
-  )
+  );
 }
 
-export default GoogleAuth
+export default GoogleAuth;
