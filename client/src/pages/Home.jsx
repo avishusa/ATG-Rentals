c081b59518633a64511f10ab279c9ab75668aa68
avish <avishusa45@gmail.com>
2024-05-14 19:36:19 -0500

    Error Solving

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -40,7 +40,7 @@ export default function Home() {
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
-        log(error);
+        console.log(error);
       }
     };
     fetchOfferListings();
@@ -55,7 +55,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          ATG Rentals is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
