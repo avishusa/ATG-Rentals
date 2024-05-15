dae822d225c74ace7b40a5cd5d397e24a21e7761
avish <avishusa45@gmail.com>
2024-05-15 18:38:56 -0500

    Deployment Prep Client Side

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
+import path from 'path';
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  const __dirname=path.resolve()
 
 const app = express();
 app.use(express.json());
@@ -33,6 +36,11 @@ app.get("/test", (req, res) => {
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
 app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
