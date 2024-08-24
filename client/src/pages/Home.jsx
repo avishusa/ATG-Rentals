7ca7c634e05fd2e23bef8c5d9b3992f336953497
avish <avishusa45@gmail.com>
2024-08-24 15:23:49 -0500

    name cahnge

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -54,7 +54,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          ATG Rentals is the best place to find your next perfect place to
+          RSP is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
